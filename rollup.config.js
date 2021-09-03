2b7b73cf4c74613d94756e4212e858d9d7ab746d
ThroughTheNight <jmethods@163.com>
2021-09-03 21:43:44 +0800

    fix: vue 2.x 不支持最新版的 rollup-plugin-vue

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
 
 import vue from 'rollup-plugin-vue';
+import commonjs from '@rollup/plugin-commonjs';
 import clear from 'rollup-plugin-clear'
 import { uglify } from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/index.js',
   output: {
-    format: 'es',
-    file: 'dist/VueLinesEllipsis.js'
+    format: 'cjs',
+    file: 'dist/vue-lines-ellipsis.js'
   },
   plugins: [
     vue(),
+    commonjs(),
+    clear({
+      targets: ['dist'],
+      watch: true,
+    }),
     // 压缩代码
     uglify(),
     // // 热更新 默认监听根文件夹
@@ -21,10 +27,6 @@ export default {
     //   port: 8000,
     //   openPage: '/public/index.html', // 打开的页面
     //   contentBase: ''
-    // }),
-    clear({
-      targets: ['dist'],
-      watch: true,
-    })
+    // })
   ]
 };
